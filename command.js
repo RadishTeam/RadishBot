ea41c236f94c8cbfa20ad58008ce5e83859ee8f4
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-10-04 19:37:31 +0800

    Update command.js

--- command.js
+++ command.js
@@ -1,51 +1,49 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const chalk = require('chalk');
-
-const commands = [
-{
-    name: 'help', 
-    description: '呼叫指令列表'
-},
-{
-    name: 'botping', 
-    description: '機器人的延遲'
-},
-{
-    name: 'ping', 
-    description: '回覆您pong！'
-},
-{
-    name: 'serverinfo', 
-    description: '關於伺服器的資訊'
-},
-{
-    name: 'userinfo', 
-    description: '關於用戶的資訊'
-},
-{
-    name: 'botinfo', 
-    description: '關於機器人的資訊'
-},
-{
-    name: 'say', 
-    description: '讓機器人說出要求的話',
-},]; 
+const logincommand = () => {
+	const commands = [
+	{
+    	name: 'help', 
+    	description: '呼叫指令列表'
+	},
+	{
+    	name: 'botping', 
+    	description: '機器人的延遲'
+	},
+	{
+    	name: 'ping', 
+    	description: '回覆您pong！'
+	},
+	{
+    	name: 'serverinfo', 
+    	description: '關於伺服器的資訊'
+	},
+	{
+    	name: 'userinfo', 
+    	description: '關於用戶的資訊'
+	},
+	{
+    	name: 'botinfo', 
+    	description: '關於機器人的資訊'
+	},]; 
 
 
-const rest = new REST({ version: '9' }).setToken('token');
+	const rest = new REST({ version: '9' }).setToken('token');
 
-(async () => {
-  try {
-    console.log(chalk.green('指令註冊 ') + '正在註冊機器人的斜線 (/) 指令');
+	(async () => {
+  	try {
+    	console.log(chalk.green('指令註冊 ') + '正在註冊機器人的斜線 (/) 指令');
 
-    await rest.put(
-      Routes.applicationCommands(''),
-      { body: commands },
-    );
+    	await rest.put(
+      	Routes.applicationCommands("891195320690700299"),
+      	{ body: commands },
+    	);
 
-    console.log(chalk.green('指令註冊 ') + '成功註冊機器人的斜線 (/) 指令');
-  } catch (error) {
-    console.error(error);
-  }
-})();
+    	console.log(chalk.green('指令註冊 ') + '成功註冊機器人的斜線 (/) 指令');
+  	} catch (error) {
+    	console.error(error);
+  	}
+	})();
+}
+module.exports = logincommand;
