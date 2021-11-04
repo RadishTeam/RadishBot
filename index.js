aa2eb0f2c9ee73bd08a92701b86453a52f0e3bec
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-04 18:44:26 +0800

    修改機器人主程式檔案
    2021 11 04 | Update by Winson_章魚#9418

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const chalk = require('chalk');
-console.log(chalk.magenta('----------------啟動區-----------------'));
+const fs = require('fs')
+/*
+console.log = (...args) => {
+  fs.appendFileSync("log.txt", args.join(" \n"))
+  process.stdout.write(args.join(" ") + "\n")
+}
+*/
+
+console.log(chalk.magenta('----------------啟動訊息區-----------------'));
 console.log(chalk.red('開始啟動 ') + '機器人已開始註冊指令並啟動');
 
-require('./command')();
+require('./sub-command')();
 require('./bot')();
