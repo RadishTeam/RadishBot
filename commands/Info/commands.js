b9248921204435c531ff83fd5cacf442feabf466
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-07 12:48:03 +0800

    指令檔案新增 | 2021 11 07 Update by Winson_章魚#9418

---
+++ commands/Info/commands.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('commands')
+	.setDescription('機器人會告訴您機器人的指令清單')
+
+module.exports.data = data
