81c3bb7dd56d4f123118ff4a4d2c668dc2f93492
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-08 20:14:02 +0800

    修改TOKEN及ID獲取途徑 | 2021 11 08 Update

--- sub_command.js
+++ sub_command.js
@@ -1,11 +1,19 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const {
+    REST
+} = require('@discordjs/rest');
+const {
+    Routes
+} = require('discord-api-types/v9');
 const chalk = require('chalk');
 const fs = require('fs');
 const logincommand = () => {
-    const commands = [];    
+    const {
+        token,
+        clientID
+    } = require('./config.json');
+    const commands = [];
     const cmdDirs = fs.readdirSync('./commands');
-	for (const dir of cmdDirs) {
+    for (const dir of cmdDirs) {
         const commandFiles = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {
             const command = require(`./commands/${dir}/${file}`);
@@ -13,17 +21,20 @@ const logincommand = () => {
         }
     }
 
-    const rest = new REST({ version: '9' }).setToken("TOKEN");
+    const rest = new REST({
+        version: '9'
+    }).setToken(token);
 
     (async () => {
         try {
             console.log(chalk.red("斜線指令") + ' 開始註冊機器人互動應用程式指令 (/) Slash command');
-			console.log(chalk.red("斜線指令") + ' 正在新增斜線指令檔案');
+            console.log(chalk.red("斜線指令") + ' 正在新增斜線指令檔案');
             await rest.put(
-                Routes.applicationCommands("CLIENT ID"),                
-                { body: commands },
+                Routes.applicationCommands(clientID), {
+                    body: commands
+                },
             );
-			console.log(chalk.red("斜線指令") + ' 成功註冊機器人互動應用程式指令 (/) Slash command');
+            console.log(chalk.red("斜線指令") + ' 成功註冊機器人互動應用程式指令 (/) Slash command');
         } catch (error) {
             console.error(error);
         }
