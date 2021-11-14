b5dd24b33758a8e15ba717521a9ad83d7edfbe72
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:12:25 +0800

    Create bullshitter.js

---
+++ commands/Play/bullshitter.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('bullshitter')
+	.setDescription('產生一段唬爛的文字')
+	.addStringOption(option =>
+		option.setName('topic')
+			.setDescription('主題')
+			.setRequired(true));
+
+module.exports.data = data
