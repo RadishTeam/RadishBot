34981da54ae7a7beafacb553c7e3ce3a719640e9
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:12:54 +0800

    Create mora.js

---
+++ commands/Play/mora.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('mora')
+	.setDescription('和機器人玩剪刀石頭布遊戲')
+	.addStringOption(option =>
+		option.setName('choose')
+			.setDescription('你要出什麼')
+			.setRequired(true)
+			.addChoice('剪刀', 'sci')
+			.addChoice('石頭', 'sto')
+            .addChoice('布', 'pap'));
+
+module.exports.data = data
