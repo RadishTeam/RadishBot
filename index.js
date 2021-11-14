a4e60501480c31ffd6f97b4c6cd7cdacb60a4727
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2021-11-15 07:04:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ console.log = (...args) => {
 */
 console.log(chalk.magenta('----------------啟動訊息區-----------------'));
 console.log(chalk.red('開始啟動 ') + '機器人已開始啟動');
-const cmdRun = false
 const botRun = true
 
-if (cmdRun) {
-    require('./sub_command')();
-}
 if (botRun) {
     require('./bot')();
 }
