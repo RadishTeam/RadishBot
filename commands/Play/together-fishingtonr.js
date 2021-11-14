f7634538d73ba975251f605b0c9e1e64f338e377
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:16:47 +0800

    Create together-fishingtonr.js

---
+++ commands/Play/together-fishingtonr.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('together-fishingtonr')
+	.setDescription('和朋友一起在語音頻道玩Fishington遊戲');
+
+module.exports.data = data
