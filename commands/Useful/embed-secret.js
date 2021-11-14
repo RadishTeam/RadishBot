481d440577ac124f327c085193f35e5fc40654a8
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:22:25 +0800

    Create embed-secret.js

---
+++ commands/Useful/embed-secret.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('embed-secret')
+	.setDescription('要求機器人傳送指定嵌入內容並不顯示指令使用者')
+	.addStringOption(option =>
+		option.setName('title')
+			.setDescription('嵌入標題')
+			.setRequired(true))
+	.addStringOption(option =>
+		option.setName('content')
+			.setDescription('嵌入內容')
+			.setRequired(true));
+
+module.exports.data = data
