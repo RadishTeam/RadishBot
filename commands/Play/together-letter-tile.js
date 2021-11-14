c2e734327a3657fb36e398c04f850a24cfe86b93
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:17:12 +0800

    Create together-letter-tile.js

---
+++ commands/Play/together-letter-tile.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('together-letter-tile')
+	.setDescription('和朋友一起在語音頻道玩Letter Tile遊戲');
+
+module.exports.data = data
