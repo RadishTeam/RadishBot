6221c52598fe7b7b24a964b1360c885a176281a7
WinsonOTP <89582370+WinsonOTP@users.noreply.github.com>
2021-11-14 12:14:43 +0800

    Create together-awkword.js

---
+++ commands/Play/together-awkword.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder() 
+	.setName('together-awkword')
+	.setDescription('和朋友一起在語音頻道玩Awkword遊戲');
+
+module.exports.data = data
