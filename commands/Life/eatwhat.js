843ff0a2ae85e5c65df2bd3ae778ef4a267cefbf
ESLint & Prettier <lint-action@samuelmeuli.com>
2021-11-24 13:20:48 +0000

    使用 ESLint 改進代碼質量

--- commands/Life/eatwhat.js
+++ commands/Life/eatwhat.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const data = new SlashCommandBuilder()
-  .setName("eatwhat")
-  .setDescription("讓機器人提供意見：要吃什麼")
-  .addNumberOption((option) =>
-    option.setName("money").setDescription("你擁有的預算").setRequired(true)
-  );
+    .setName('eatwhat')
+    .setDescription('讓機器人提供意見：要吃什麼')
+    .addNumberOption((option) =>
+        option.setName('money').setDescription('你擁有的預算').setRequired(true),
+    );
 
 module.exports = { data };
--- commands/Owner/presence.js
+++ commands/Owner/presence.js
@@ -1,16 +1,16 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const data = new SlashCommandBuilder()
-  .setName("presence")
-  .setDescription("僅機器人擁有者可用的指令 (更改機器人的狀態)")
-  .addStringOption((option) =>
-    option
-      .setName("presence")
-      .setDescription("狀態選擇")
-      .setRequired(true)
-      .addChoice("在線", "online")
-      .addChoice("閒置", "idle")
-      .addChoice("勿擾", "dnd")
-      .addChoice("隱形", "invisible")
-  );
+    .setName('presence')
+    .setDescription('僅機器人擁有者可用的指令 (更改機器人的狀態)')
+    .addStringOption((option) =>
+        option
+            .setName('presence')
+            .setDescription('狀態選擇')
+            .setRequired(true)
+            .addChoice('在線', 'online')
+            .addChoice('閒置', 'idle')
+            .addChoice('勿擾', 'dnd')
+            .addChoice('隱形', 'invisible'),
+    );
 module.exports = { data };
