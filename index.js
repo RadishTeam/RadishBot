842d55e6a30abb3b0975ffa7158b27dd6f1c633b
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2021-12-05 11:25:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const chalk = require('chalk');
-// const fs = require('fs')
-/*
-console.log = (...args) => {
-  fs.appendFileSync("log.txt", args.join(" \n"))
-  process.stdout.write(args.join(" ") + "\n")
-}
-*/
-console.log(chalk.magenta('----------------啟動訊息區-----------------'));
-console.log(chalk.red('開始啟動 ') + '機器人已開始啟動');
 
 try {
     // Config
@@ -15,6 +6,19 @@ try {
 } catch (e) {
     console.log(e);
 }
+
+// const fs = require('fs')
+/*
+console.log = (...args) => {
+  fs.appendFileSync("log.txt", args.join(" \n"))
+  process.stdout.write(args.join(" ") + "\n")
+}
+*/
+
+console.log(chalk.magenta('----------------啟動訊息區-----------------'));
+
+if (!this.config.ci) return console.log(chalk.red('開始啟動 ') + '機器人已開始啟動');
+console.log(chalk.red('CI測試事件 ') + 'CI正在測試機器人');
 
 if (!this.config.token) throw new TypeError('沒有找到 token! 請在 config.js 中輸入您的token!');
 
