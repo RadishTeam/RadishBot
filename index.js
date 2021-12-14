84f301cee2929f415b0bcad54e425b7851c5e78c
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2021-12-15 06:25:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ console.log = (...args) => {
 
 console.log(chalk.magenta('----------------啟動訊息區-----------------'));
 
-if (this.config.CI === 'true') console.log(chalk.red('CI測試事件 ') + 'CI正在進行測試');
+const ci = process.env.CI
+if (CI === 'true') console.log(chalk.red('CI測試事件 ') + 'CI正在進行測試');
 console.log(chalk.red('開始啟動 ') + '機器人已開始啟動');
 
 if (!this.config.token) throw new TypeError('沒有找到 token! 請在 config.js 中輸入您的token!');
